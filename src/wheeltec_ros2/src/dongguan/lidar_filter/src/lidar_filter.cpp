ee37435a70a496921eccced631a151e474476991
Wouter <3747759+wouter1602@users.noreply.github.com>
2025-12-10 14:38:27 +0100

    Added 2 blindspot with correct angle

--- src/wheeltec_ros2/src/dongguan/lidar_filter/src/lidar_filter.cpp
+++ src/wheeltec_ros2/src/dongguan/lidar_filter/src/lidar_filter.cpp
@@ -10,9 +10,13 @@ class ScanFilter : public rclcpp::Node
 public:
     ScanFilter() : Node("scan_filter")
     {
-        // Blindspot (in radians) – pas aan wat je wilt
-        blind_start_ = 160.0 * M_PI / 180.0;  // beginhoek blindspot
-        blind_end_ = 240.0 * M_PI / 180.0;    // eindhoek blindspot
+        // Blindspot in Radian. Change to the angle you need.
+        // When changing these values you need to run "colcon build" to recompile
+        blind_1_start_ = 317.0 * M_PI / 180.0;  // Start blindspot (in degrees)
+        blind_1_end_ = 337.0 * M_PI / 180.0;    // End blindstpot (in degrees)
+
+        blind_2_start_ = 23.0 * M_PI / 180.0;
+        blind_2_end_ = 43.0 * M_PI / 180.0;
 
         // Abonneer op de ruwe LiDAR data
         subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
@@ -23,8 +27,8 @@ public:
 
         RCLCPP_INFO(this->get_logger(),
                     "ScanFilter actief — blindspot van %.1f° tot %.1f°",
-                    blind_start_ * 180.0 / M_PI,
-                    blind_end_ * 180.0 / M_PI);
+                    blind_1_start_ * 180.0 / M_PI,
+                    blind_1_end_ * 180.0 / M_PI);
     }
 
 private:
@@ -48,9 +52,13 @@ private:
         // Loop om blindspots blind te maken
         for (size_t i = 0; i < msg->ranges.size(); i++)
         {
-            if (angle >= blind_start_ && angle <= blind_end_)
+            //blindspot 1
+            if (angle >= blind_1_start_ && angle <= blind_1_end_)
             {
-                filtered.ranges[i] = std::numeric_limits<float>::infinity();  // niets gezien
+                filtered.ranges[i] = std::numeric_limits<float>::infinity();  // Sets range data to infinite if it should not see anything
+            } else if (angle >= blind_2_start_ && angle <= blind_2_end_) //blindspot 2
+            {
+                filtered.ranges[i] = std::numeric_limits<float>::infinity();  // Sets range data to infinite if it should not see anything
             }
             angle += msg->angle_increment;
         }
@@ -60,8 +68,10 @@ private:
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr publisher_;
-    double blind_start_;
-    double blind_end_;
+    double blind_1_start_;
+    double blind_1_end_;
+    double blind_2_start_;
+    double blind_2_end_;
 };
 
 int main(int argc, char * argv[])
