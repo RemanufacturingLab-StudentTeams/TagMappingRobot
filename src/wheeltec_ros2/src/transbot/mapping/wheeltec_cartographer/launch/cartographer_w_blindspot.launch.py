6b193d01206734e016df3fe688a88a82bb9b74a5
Wouter <3747759+wouter1602@users.noreply.github.com>
2025-12-11 14:00:25 +0100

    Made cartographer launch file to start lidar with filtered data

---
+++ src/wheeltec_ros2/src/transbot/mapping/wheeltec_cartographer/launch/cartographer_w_blindspot.launch.py
@@ -0,0 +1,116 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration("use_sim_time", default="False")
+
+    cartographer_config_dir = LaunchConfiguration(
+        "cartographer_config_dir",
+        default=os.path.join(
+            get_package_share_directory("wheeltec_cartographer"), "config"
+        ),
+    )
+    configuration_basename = LaunchConfiguration(
+        "configuration_basename", default="cartographer.lua"
+    )
+
+    resolution = LaunchConfiguration("resolution", default="0.05")
+    publish_period_sec = LaunchConfiguration("publish_period_sec", default="0.5")
+
+    bringup_dir = get_package_share_directory("turn_on_wheeltec_robot")
+    launch_dir = os.path.join(bringup_dir, "launch")
+
+    filter_dir = get_package_share_directory("lidar_filter")
+    launch_filter_dir = os.path.join(filter_dir, "launch")
+
+    wheeltec_robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, "turn_on_wheeltec_robot.launch.py")
+        ),
+        launch_arguments={"carto_slam": "true"}.items(),
+    )
+    wheeltec_lidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, "wheeltec_lidar.launch.py")
+        ),
+    )
+
+    lidar_filter = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_filter_dir, "lidar_filter.launch.py")
+        )
+    )
+    return LaunchDescription(
+        [
+            wheeltec_robot,
+            wheeltec_lidar,
+            lidar_filter,
+            DeclareLaunchArgument(
+                "cartographer_config_dir",
+                default_value=cartographer_config_dir,
+                description="Full path to config file to load",
+            ),
+            DeclareLaunchArgument(
+                "configuration_basename",
+                default_value=configuration_basename,
+                description="Name of lua file for cartographer",
+            ),
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="false",
+                description="Use simulation (Gazebo) clock if true",
+            ),
+            DeclareLaunchArgument(
+                "resolution",
+                default_value=resolution,
+                description="Resolution of a grid cell in the published occupancy grid",
+            ),
+            DeclareLaunchArgument(
+                "publish_period_sec",
+                default_value=publish_period_sec,
+                description="OccupancyGrid publishing period",
+            ),
+            Node(
+                package="cartographer_ros",
+                executable="cartographer_node",
+                name="cartographer_node",
+                parameters=[{"use_sim_time": use_sim_time}],
+                arguments=[
+                    "-configuration_directory",
+                    cartographer_config_dir,
+                    "-configuration_basename",
+                    configuration_basename,
+                ],
+                remappings=[
+                    ("odom", "odom"),
+                    ("imu", "/mobile_base/sensors/imu_data"),
+                    ("scan", "scanned"),
+                ],
+            ),
+            Node(
+                package="cartographer_ros",
+                executable="cartographer_occupancy_grid_node",
+                name="occupancy_grid_node",
+                parameters=[{"use_sim_time": use_sim_time}],
+                arguments=[
+                    "-resolution",
+                    resolution,
+                    "-publish_period_sec",
+                    publish_period_sec,
+                ],
+            ),
+        ]
+    )
